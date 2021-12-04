9f0a8c9c7d6962f86e6886acb1113f188fc0601e
Rafael Mantovani Espindola Pessoa <rafael.pessoa@codex.com.br>
2021-12-04 19:09:14 -0300

    [mutual-exclusion] Implementing mutual exclusion algorithm

---
+++ mutual-exclusion/peerMutualExclusion.py
@@ -0,0 +1,57 @@
+from xmlrpc import server
+from xmlrpc.server import SimpleXMLRPCServer
+import xmlrpc.client
+import sys
+import time
+import threading
+import random
+import urllib.request
+
+
+# Program parameters
+# 0 -> program name
+# 1 -> server adress
+# 2 -> next peer adress
+
+server_address = sys.argv[1]
+next_peer_address = sys.argv[2]
+external_address = urllib.request.urlopen('https://api.ipify.org').read().decode('utf8')
+
+hasKey = True if (external_address > next_peer_address) else False
+
+def receive_key():
+    global hasKey
+    hasKey = True
+
+def pass_key():
+    global hasKey
+    if (hasKey):
+        hasKey = False
+        next_peer.receive_key()
+
+def sum_1_variable():
+    common_variable = server.get_common_variable()
+    server.set_common_variable(common_variable + 1)
+
+local_server = SimpleXMLRPCServer(("", 8000), allow_none=True, logRequests=False)
+local_server.register_function(receive_key)
+server_thread = threading.Thread(target=local_server.serve_forever)
+server_thread.start()
+
+print("Starting client...")
+# with xmlrpc.client.ServerProxy(hosts) as proxy:
+next_peer = xmlrpc.client.ServerProxy(next_peer_address)
+server =  xmlrpc.client.ServerProxy(server_address)
+
+i = 0
+
+# while i < 3000:
+#     sum_1_variable()
+#     i += 1
+
+print(hasKey)
+while i < 3000:
+    if (hasKey):
+        sum_1_variable()
+        i+= 1
+        pass_key()
---
+++ mutual-exclusion/serverMutualExclusion.py
@@ -0,0 +1,26 @@
+from xmlrpc.server import SimpleXMLRPCServer
+import xmlrpc.client
+import sys
+import time
+import threading
+import random
+
+common_variable = 0
+set_count = 0
+
+def get_common_variable():
+    return common_variable
+
+def set_common_variable(value):
+    global set_count, common_variable
+    set_count += 1 
+    common_variable = value
+
+server = SimpleXMLRPCServer(("", 8000), allow_none=True, logRequests=False) 
+server.register_function(get_common_variable)
+server.register_function(set_common_variable)
+server_thread = threading.Thread(target=server.serve_forever)
+server_thread.start()
+
+time.sleep(15)
+print("Valor esperado: ", set_count, "Valor real: ", common_variable)
