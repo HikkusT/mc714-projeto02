b3e11f07e5b9a8efcdbf4c500cb503beabaaa286
Luiz Alves (Hik) <luiz.alves@playkids.com>
2021-12-05 13:42:32 -0300

    Add fence for all peers to be connected

--- lamport/broadcast.py
+++ lamport/broadcast.py
@@ -5,13 +5,24 @@ import time
 import threading
 import random
 
-id = sys.argv[1]
+my_id = int(sys.argv[1])
+peer_addresses = sys.argv[2].split(',')
+number_of_peers = len(peer_addresses)
+my_port = int(sys.argv[3]) if len(sys.argv) > 3 else 8000
+
+# Constants
+number_of_iterations = 4
+min_sleep_time = 5
+max_sleep_time = 15
+
+# Local state
 current_time = 0
 events = []
 
 def log_event(time, id):
     events.append((time, id))
 
+# Listeners
 def receive_event(time, id):
     global current_time
     current_time = max(time, current_time) + 1
@@ -19,37 +30,54 @@ def receive_event(time, id):
     log_event(time, id)
     print("Received from " + str(id) + " with timestamp " + str(time))
 
-def send_event(hosts):
-    global current_time, id
+def health_check():
+    return True
+
+
+# Senders
+def broadcast_event(peers):
+    global current_time
     current_time += 1
 
-    log_event(current_time, id)
+    log_event(current_time, my_id)
     print("Broadcasting at " + str(current_time))
 
-    for host in hosts:
-        host.receive_event(current_time, id)
+    for peer in peers:
+        peer.receive_event(current_time, my_id)
+
+def wait_all_peers_ready(peers):
+    non_ready_peers = peers.copy()
+    while len(non_ready_peers) != 0:
+        for non_ready_peer in non_ready_peers:
+            try:
+                non_ready_peer.health_check()
+                non_ready_peers.remove(non_ready_peer)
+            except:
+                pass
     
 
-server = SimpleXMLRPCServer(("", int(sys.argv[3])), allow_none=True, logRequests=False) 
+# Server stuff
+server = SimpleXMLRPCServer(("", my_port), allow_none=True, logRequests=False) 
 server.register_function(receive_event)
+server.register_function(health_check)
 server_thread = threading.Thread(target=server.serve_forever)
 server_thread.start()
 
-hostsAddresses = sys.argv[2].split(',')
-hosts = []
-print("Starting client...")
-# with xmlrpc.client.ServerProxy(hosts) as proxy:
-for hostAddress in hostsAddresses:
-    hosts.append(xmlrpc.client.ServerProxy(hostAddress))
+# Client stuff
+peers = [xmlrpc.client.ServerProxy(peer_address) for peer_address in peer_addresses]
 
-for i in range(4):
-    time_to_sleep = random.randint(5, 15)
+print("Going to wait")
+wait_all_peers_ready(peers)
+print("All peers ready")
+
+for iteration in range(number_of_iterations):
+    time_to_sleep = random.randint(min_sleep_time, max_sleep_time)
     print("Sleeping for " + str(time_to_sleep))
     time.sleep(time_to_sleep)
 
-    send_event(hosts)
+    broadcast_event(peers)
 
-time.sleep(15)
+time.sleep(max_sleep_time)
 print("Order: ")
 events.sort(key= lambda event: event[0])
 for event in events:
