1bdf14986becf8c59d2bc0a650e5ce69ecbbaabb
Rafael Mantovani Espindola Pessoa <rafael.pessoa@codex.com.br>
2021-12-05 18:29:25 -0300

    [election] Fixing some bugs on election algorithm and creating a file for local testing.

--- election/electionPeerTestLocal.py
+++ election/electionPeerTestLocal.py
@@ -8,34 +8,48 @@ import time
 # Program parameters
 # 0 -> program name
 # 1 -> peer adress list
+# 2 -> local port                
 
 def start_election():
-    print("Peer ", local_peer_index, " is starting an election.")
+    global in_the_middle_of_election
+    in_the_middle_of_election = True
+    print("\nPeer", local_peer_index, "is starting an election.")
     anyone_more_powerfull_answered = False
-    for i in range(local_peer_index, len(all_peers_list)):
+    for i in range(local_peer_index + 1, len(all_peers_list)):
         try:
             anyone_more_powerfull_answered = all_peers_list[i].receive_election_request()
+            in_the_middle_of_election = False
             return
-        #Tried to communicate with a down peer.
         except:
-            print("Peer ", i, "is down.")
+            print("Peer", i, "is down.")
     
-    announce_as_coordinator()
+    in_the_middle_of_election = False
+    print()
+    announce_as_leader()
 
 def receive_election_request():
     try:
         return True
     finally:
-        start_election()
+        if not in_the_middle_of_election:
+            start_election()
 
-def announce_as_coordinator():
-    for peer in all_peers_list:
-        peer.receive_coordinator_announcement(local_peer_index)
+def announce_as_leader():
+    print("Annoucing me as the leader.")
+    global leader_index
+    leader_index = local_peer_index
+    for i in range(len(all_peers_list)):
+        if i != local_peer_index:
+            try:
+                all_peers_list[i].receive_leader_announcement(local_peer_index)
+                print("Peer", i, "acknowledge your power.")
+            except:
+                print("Peer", i, "is not alive to acknowledge your leadership.")
 
-def receive_coordinator_announcement(coordinator_index):
-    global coordinator
-    coordinator = all_peers_list[coordinator_index]
-    print("Peer ", coordinator_index, " is the new coordinator.")
+def receive_leader_announcement(new_leader_index):
+    global leader_index
+    leader_index = new_leader_index
+    print("\nPeer", leader_index, "is the new leader.")
 
 def receive_status_request():
     return True
@@ -48,36 +62,47 @@ def print_all_peers_ordered_list():
 # external_address = urllib.request.urlopen('https://api.ipify.org').read().decode('utf8')
 # external_address = "http://" + external_address + ":8000/"
 
+print()
+local_peer_port = sys.argv[2]
 others_peers_address_list = sys.argv[1].split(',')
+external_address = others_peers_address_list[0][:-4] + local_peer_port 
+print(external_address)
+
 others_peers_address_list.append(external_address)
-all_peers_address_list = others_peers_address_list.sorted()
+all_peers_address_list = sorted(others_peers_address_list)
 local_peer_index = all_peers_address_list.index(external_address)
 
 #Startin local server
-local_server = SimpleXMLRPCServer(("", 8000 + local_peer_index), allow_none=True, logRequests=False)
-local_server.register_function(receive_coordinator_announcement)
+local_server = SimpleXMLRPCServer(("", int(local_peer_port)), allow_none=True, logRequests=False)
+local_server.register_function(receive_leader_announcement)
 local_server.register_function(receive_election_request)
+local_server.register_function(receive_status_request)
 
 server_thread = threading.Thread(target=local_server.serve_forever)
 server_thread.start()
 
+# time.sleep(5)
 # with xmlrpc.client.ServerProxy(hosts) as proxy:
 all_peers_list = [xmlrpc.client.ServerProxy(address) for address in all_peers_address_list]
+in_the_middle_of_election = False
 
-
-print("I'm peer number ", local_peer_index, ".")
+print("I'm peer number", local_peer_index, ".")
 print_all_peers_ordered_list()
-coordinator_index = len(all_peers_list) - 1
+leader_index = len(all_peers_list) - 1
 
 time.sleep(5)
 
 start_election()
 
-time.sleep(5)
+while True:
+    time.sleep(5)
+    if (leader_index != local_peer_index):
+        try:
+            all_peers_list[leader_index].receive_status_request()
+            print("\nPeer", leader_index, "is ok.")
+        except:
+            print("\nPeer", leader_index, "found dead at the scene.\n")
+            start_election()
+    else:
+        print("\nI'm the leader, everything is alright.")
 
-try: 
-    all_peers_list[coordinator_index].receive_status_request()
-    print("Peer ", coordinator_index, "is ok.")
-except:
-    print("Peer ", coordinator_index, "found dead at the scene.")
-    start_election()
